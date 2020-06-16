b68a4c4b64a5a3fd541384dc3d8f09e2f7eec510
Hannah Livnat <hannahlivnat@gmail.com>
2020-06-16 16:01:40 -0700

    added controller directory

---
+++ controllers/beans.js
@@ -0,0 +1,20 @@
+const express = router('express');
+const router = express.Router();
+
+// const Beans = requires('../models/bean.js');
+
+//INDEX
+// router.get('/', (req, res) => {
+//   Beans.find({}, (err, foundBeans) => {
+//     res.json(foundBeans);
+//   })
+// });
+
+//CREATE
+
+//DELETE
+
+//EDIT
+
+//export 
+module.exports = router;
